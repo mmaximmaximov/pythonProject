a0c571ecc5c5d08fb3adca61adbbbb34ea9329a3
Maxim <mmaximmaximovgmail.com@MacBook-Air-Maxim-2.local>
2023-02-10 17:04:20 +0300

    Add

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 age = 20
-3name = 'Marsel'
+name = 'Marsel'
 text = f'Hello, my name is {name} i am {age} y.o'
 print(text)
 
+a = 10
+b = 10.001
 
+a_f = float(a)
+test = f'a={a_f}, b={b}'
+test_f = f'a={a:2.4f}, b={b:2.4f}'
+print(test)
+print(test_f)
